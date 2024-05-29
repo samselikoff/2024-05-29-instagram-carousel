4cdcf8a989902a974556316fdf3602ec8029c2c4
Sam Selikoff <sam.selikoff@gmail.com>
2024-05-29 15:39:22 -0400

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} flex flex-col justify-center items-center h-full`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,118 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+
+let images = [
+  { id: 1, className: "bg-green-500" },
+  { id: 2, className: "bg-blue-500" },
+  { id: 3, className: "bg-red-500" },
+  { id: 4, className: "bg-orange-500" },
+  { id: 5, className: "bg-purple-500" },
+  { id: 6, className: "bg-green-500" },
+  { id: 7, className: "bg-blue-500" },
+  { id: 8, className: "bg-red-500" },
+];
 
 export default function Home() {
+  let [activeIndex, setActiveIndex] = useState(0);
+  let [activeSlot, setActiveSlot] = useState(activeIndex);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <div className="flex items-center">
+        <button
+          className="size-10"
+          onClick={() => {
+            setActiveIndex(activeIndex - 1);
+            if (activeSlot > 0) {
+              setActiveSlot(activeSlot - 1);
+            }
+          }}
+        >
+          &lt;
+        </button>
+        <div className="size-40 ring-2 ring-red-500 overflow-hidden">
+          <div
+            className="flex h-40 relative transition-[translate]"
+            style={{ translate: `-${activeIndex * 100}%` }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {images.map((image) => (
+              <div
+                key={image.id}
+                className={`h-full aspect-square ${image.className}`}
+              />
+            ))}
+          </div>
         </div>
+
+        <button
+          className="size-10"
+          onClick={() => {
+            setActiveIndex(activeIndex + 1);
+            if (activeSlot < 2) {
+              setActiveSlot(activeSlot + 1);
+            }
+          }}
+        >
+          &gt;
+        </button>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-8 flex justify-center gap-1">
+        {images.map((image, index) => (
+          <Dot
+            key={image.id}
+            image={image}
+            index={index}
+            activeIndex={activeIndex}
+            activeSlot={activeSlot}
+          />
+        ))}
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mt-8">
+        <p>length: {images.length}</p>
+        <p>activeIndex: {activeIndex}</p>
+        <p>activeSlot: {activeSlot}</p>
       </div>
-    </main>
+    </div>
+  );
+}
+
+function Dot({ image, index, activeIndex, activeSlot }) {
+  let scale = 0.25;
+
+  if (activeSlot === 0) {
+    if (index - activeIndex > -1 && index - activeIndex < 3) {
+      scale = 1;
+    } else if (activeIndex - index === 1) {
+      scale = 0.75;
+    } else if (index - activeIndex === 3) {
+      scale = 0.75;
+    }
+  } else if (activeSlot === 1) {
+    if (Math.abs(index - activeIndex) < 2) {
+      scale = 1;
+    } else if (Math.abs(activeIndex - index) === 2) {
+      scale = 0.75;
+    }
+  } else if (activeSlot === 2) {
+    if (activeIndex - index < 3 && activeIndex - index > -1) {
+      scale = 1;
+    } else if (index - activeIndex === 1) {
+      scale = 0.75;
+    } else if (activeIndex - index === 3) {
+      scale = 0.75;
+    }
+  }
+
+  return (
+    <div
+      className={`size-5 rounded-full transition-[scale] ${index === activeIndex ? "bg-blue-500" : "bg-gray-500"}`}
+      style={{
+        scale,
+      }}
+    />
   );
 }
